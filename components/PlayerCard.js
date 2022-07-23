153cae2860a6bcc373984326302decbe31b8ecda
Christian Black <blackl3@gmail.com>
2022-07-23 12:56:09 -0500

    Built READ API Call for Players and Teams as well as Player Card

---
+++ components/PlayerCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
+import Link from 'next/link';
+
+export default function PlayerCard({ playerObj, onUpdate }) {
+  const deleteThisPlayer = () => {
+    if (window.confirm(`Delete ${playerObj.name}?`)) {
+      // eslint-disable-next-line no-undef
+      deletePlayer(playerObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={playerObj.imageURL} alt={playerObj.imageURL} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{playerObj.name}</Card.Title>
+        <p>{playerObj.position}</p>
+        {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+        <Link href={`/player/${playerObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/player/edit/${playerObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisPlayer} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+PlayerCard.propTypes = {
+  playerObj: PropTypes.shape({
+    imageURL: PropTypes.string,
+    name: PropTypes.string,
+    position: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
