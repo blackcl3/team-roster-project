7ab79d9e8e78bffb98ce78736ad500581303e888
Christian Black <blackl3@gmail.com>
2022-07-23 13:01:58 -0500

    fixed linter error and made sure READ Team API call works

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import getPlayers from '../api/playersData';
 import { useAuth } from '../utils/context/authContext';
 import PlayerCard from '../components/PlayerCard';
-import getTeam from '../api/teamsData';
 
 function Home() {
   const [players, setPlayers] = useState();
