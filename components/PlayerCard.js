7388e12bdb2d394538e8bde3d91df741709cfeb0
Christian Black <blackl3@gmail.com>
2022-07-24 18:43:50 -0500

    Added delete player functionality

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
 import Link from 'next/link';
+import { deletePlayer } from '../api/playersData';
 
 export default function PlayerCard({ playerObj, onUpdate }) {
   const deleteThisPlayer = () => {
