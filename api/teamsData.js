02571e4b79a19b1395bae5cdf258646d823b68eb
Christian Black <blackl3@gmail.com>
2022-07-26 20:59:57 -0500

    created API calls for CREATE and UPDATE Teams as well as integrated with forms

--- api/teamsData.js
+++ api/teamsData.js
@@ -3,10 +3,34 @@ import { clientCredentials } from '../utils/client';
 
 const dbUrl = clientCredentials.databaseURL;
 
-const getTeam = (uid) => new Promise((resolve, reject) => {
+const getTeams = (uid) => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/team.json?orderBy="uid"&equalTo="${uid}"`)
     .then((response) => resolve((Object.values(response.data))))
     .catch(reject);
 });
 
-export default getTeam;
+const getSingleTeam = (teamfirebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/team/${teamfirebaseKey}.json`)
+    .then((response) => resolve((response.data)))
+    .catch(reject);
+});
+
+const createTeam = (teamObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/team.json`, teamObj)
+    .then((response) => {
+      const payload = { teamfirebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/team/${response.data.name}.json`, payload)
+        .then(resolve);
+    })
+    .catch(reject);
+});
+
+const updateTeam = (teamObj, uid) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/team/${teamObj.teamfirebaseKey}.json`, teamObj)
+    .then(() => getTeams(uid).then(resolve))
+    .catch(reject);
+});
+
+export {
+  getTeams, getSingleTeam, createTeam, updateTeam,
+};
