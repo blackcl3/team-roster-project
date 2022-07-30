886df9902d2f4493232a428af312c325127c2367
Christian Black <blackl3@gmail.com>
2022-07-30 09:42:43 -0500

    added title to browser

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ function Home() {
   };
   useEffect(() => {
     getAllPlayers();
+    document.title = 'HomeTeam';
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
